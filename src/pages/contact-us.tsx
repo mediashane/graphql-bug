eadaac140a261afbd60d09dc2378f4f4c1ec3f55
shane-koalition <shane@koalition.com>
2021-10-22 17:11:39 +0100

    added Footer props to other pages, removed old Footer files

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -25,7 +25,22 @@ export default function Page() {
         </div>
       </main>
 
-      <Footer />
+      <Footer
+        title="Elizabeth Eakins Studio"
+        description="The Elizabeth Eakins Studio is located in South Norwalk Connecticut and is open Monday through Friday from 9am to 5:00pm. Please call or email us to visit and we would be happy to have you come by."
+        address="5 Taft Street, South Norwalk, CT 06854"
+        phoneNumber="203.831.9347"
+        email="Elizabeth.Eakins@elizabetheakins.com"
+        hours="9:00am to 5:00pm M-F"
+        locationTitle="Holland & Sherry"
+        locationDescription="Elizabeth Eakins is part of Holland & Sherry and available for purchase across their showrooms. Click below to find a location near you."
+        locationButtonLabel="Find a location near you"
+        socialTitle="Follow Elizabeth Eakins on Instagram and join our mailing list"
+        socialDescription="Many of our new products and offers we first share in email and instagram. Follow us today"
+        emailPlaceholder="Enter your email"
+        igCta="Follow us on Instagram"
+        igLink="https://instagram.com/elizabetheakins/"
+      />
     </>
   );
 }
