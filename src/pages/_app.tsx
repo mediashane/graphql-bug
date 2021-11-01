5ed2de789bb1a514d27d6f68351d4de887186994
shane-koalition <shane@koalition.com>
2021-11-01 11:05:28 -0700

    added marker.io widget

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,25 @@
 import 'faust.config';
 import 'normalize.css/normalize.css';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { client } from 'client';
 import type { AppProps } from 'next/app';
 
 import { HeadlessProvider } from '@faustjs/next';
+import markerSDK from '@marker.io/browser';
 
 export default function KoaApp({ Component, pageProps }: AppProps) {
+  const fetchWidget = async () => {
+    const widget = await markerSDK.loadWidget({
+      destination: '617c0b2163614c5811dc57a8',
+    });
+    console.log('Marker.io widget loaded: ', widget.isVisible);
+  };
+
+  useEffect(() => {
+    fetchWidget();
+  }, []);
+
   return (
     <>
       <HeadlessProvider client={client} pageProps={pageProps}>
