6b7677fda595922c6ceff6aaeb1ce83dfdb327d7
Aitor Aznar <aitor@koalition.com>
2021-11-10 19:18:20 +0100

    Allow load remote images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-const { withFaust } = require('@faustjs/next');
+const { withFaust } = require('@faustjs/next'); // eslint-disable-line
+
+const nextjsConfig = {
+  image: {
+    domains: ['placehold.it', 'wordpress-536476-2188960.cloudwaysapps.com/'],
+  },
+};
 
 /**
  * @type {import('next').NextConfig}
  **/
-module.exports = withFaust();
+module.exports = withFaust(nextjsConfig);
