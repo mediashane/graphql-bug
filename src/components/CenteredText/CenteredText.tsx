94c6d7e6e841d34f211d21f304cddeeda4d32e7a
shane-koalition <shane@koalition.com>
2021-11-16 16:11:33 -0800

    made textSize and backgroundColor type optional in CenteredText component

--- src/components/CenteredText/CenteredText.tsx
+++ src/components/CenteredText/CenteredText.tsx
@@ -6,8 +6,8 @@ import styles from './styles';
 
 interface Props {
   text: string;
-  backgroundColor: string;
-  textSize: string;
+  backgroundColor?: string;
+  textSize?: string;
 }
 
 function CenteredText({ text = '', backgroundColor = 'transparent', textSize = '16px' }: Props): JSX.Element {
