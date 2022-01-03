5829bf2386a496cd897272f401f5bd3435cb292d
shane-koalition <shane@koalition.com>
2022-01-03 13:54:28 -0800

    clean up typing for ContactBanner

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -21,18 +21,13 @@ export default function Page() {
       <ContactBanner
         title="The Elizabeth Eakins Studio"
         description="The Elizabeth Eakins Studio is located in South Norwalk Connecticut and is open Monday through Friday from 9am to 5:00pm. Please call or email us to visit and we would be happy to have you come by to learn more about us and how we can help."
-        address="Elizabeth Eakins Studio 5 Taft Street, South Norwalk, CT 06854"
+        addressOne="Elizabeth Eakins Studio"
+        addressTwo="5 Taft Street, South Norwalk, CT 06854"
         phoneNumber="203.831.9347"
+        faxNumber="203.831.9136"
         email="Elizabeth.Eakins@elizabetheakins.com"
-        hours="HOURS"
-        locationTitle="LOCATION"
-        locationDescription="DESCP"
         locationButtonLabel="Book an Appointment"
-        socialTitle="SOCIAL"
-        socialDescription="DESCP"
-        emailPlaceholder="PLACEHOLDER"
-        igCta="CTA"
-        igLink="/link"
+        image="images/showroom.jpg"
       />
 
       {/* <Footer
