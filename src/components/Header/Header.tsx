26756158c724c8022aec814078c81685a30321d5
shane-koalition <shane@koalition.com>
2022-01-05 10:42:33 -0800

    checking if window object is not defined before browser detection

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -60,7 +60,11 @@ function Header({ title = 'Elizabeth Eakins' }: Props): JSX.Element {
 
   let isSafari = false;
 
-  if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
+  if (
+    typeof window !== 'undefined' &&
+    navigator?.userAgent.indexOf('Safari') != -1 &&
+    navigator?.userAgent.indexOf('Chrome') == -1
+  ) {
     isSafari = true;
   }
 
