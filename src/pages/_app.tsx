49a924d9253bdce1f554031b2929bf69ca9e1aef
shane-koalition <shane@koalition.com>
2022-01-06 13:10:50 -0800

    testing removal of Marker add-on

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,8 @@ import 'faust.config';
 import 'koa.config';
 import 'normalize.css/normalize.css';
 
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
+import React from 'react';
 import { client } from 'client';
 import type { AppProps } from 'next/app';
 import NextNProgress from 'nextjs-progressbar';
@@ -10,20 +11,20 @@ import { colorBrandBlue } from 'style';
 import theme from 'style/theme';
 
 import { FaustProvider } from '@faustjs/next';
-import markerSDK from '@marker.io/browser';
+// import markerSDK from '@marker.io/browser';
 import { ThemeProvider } from '@mui/material';
 
 export default function KoaApp({ Component, pageProps }: AppProps) {
-  const fetchWidget = async () => {
-    const widget = await markerSDK.loadWidget({
-      destination: `617c0b2163614c5811dc57a8`,
-    });
-    console.log('Marker.io widget loaded: ', widget.isVisible);
-  };
+  // const fetchWidget = async () => {
+  //   const widget = await markerSDK.loadWidget({
+  //     destination: `617c0b2163614c5811dc57a8`,
+  //   });
+  //   console.log('Marker.io widget loaded: ', widget.isVisible);
+  // };
 
-  useEffect(() => {
-    fetchWidget();
-  }, []);
+  // useEffect(() => {
+  //   fetchWidget();
+  // }, []);
 
   return (
     <>
