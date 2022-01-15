8273ed886c0c1273ac115b9fa1a42e7c0c94a0f0
shane-koalition <shane@koalition.com>
2022-01-14 21:49:00 -0800

    fixing isolated module error?

--- src/pages/examples/index-old.tsx
+++ src/pages/examples/index-old.tsx
@@ -1,152 +1,152 @@
-// import React from 'react';
-// import { client, PageIdType } from 'client';
-// import {
-//   CallToAction,
-//   EmailSubscribe,
-//   Footer,
-//   Header,
-//   HeroLeftJustified,
-//   SaleCta,
-//   ThreeImageButtons,
-// } from 'components';
-// import ThreeFeatureCards from 'components/ThreeFeatureCards/ThreeFeatureCards';
-// import { GetStaticPropsContext } from 'next';
-// import Head from 'next/head';
+import React from 'react';
+import { client, PageIdType } from 'client';
+import {
+  CallToAction,
+  EmailSubscribe,
+  Footer,
+  Header,
+  HeroLeftJustified,
+  // SaleCta,
+  ThreeImageButtons,
+} from 'components';
+import ThreeFeatureCards from 'components/ThreeFeatureCards/ThreeFeatureCards';
+import { GetStaticPropsContext } from 'next';
+import Head from 'next/head';
 
-// import { getNextStaticProps } from '@faustjs/next';
+import { getNextStaticProps } from '@faustjs/next';
 
-// export default function Page() {
-//   const { useQuery } = client;
-//   const generalSettings = useQuery().generalSettings;
+export default function Page() {
+  const { useQuery } = client;
+  const generalSettings = useQuery().generalSettings;
 
-//   const { usePage } = client;
-//   const pageData = usePage({
-//     id: '/',
-//     idType: PageIdType.URI,
-//   });
+  const { usePage } = client;
+  const pageData = usePage({
+    id: '/',
+    idType: PageIdType.URI,
+  });
 
-//   const { textOverline, textHeadline, textParagraph, media } =
-//     pageData.pageBuilder.modules[0].$on.Page_Pagebuilder_Modules_HeroLeftJustified;
-//   const { mediaItemUrl } = media;
+  const { textOverline, textHeadline, textParagraph, media } =
+    pageData.pageBuilder.modules[0].$on.Page_Pagebuilder_Modules_HeroLeftJustified;
+  const { mediaItemUrl } = media;
 
-//   return (
-//     <>
-//       <Header />
-//       <Head>
-//         <title>Rugs - {generalSettings.title}</title>
-//       </Head>
+  return (
+    <>
+      <Header />
+      <Head>
+        <title>Rugs - {generalSettings.title}</title>
+      </Head>
 
-//       <HeroLeftJustified
-//         textOverline={textOverline}
-//         textHeadline={textHeadline}
-//         textParagraph={textParagraph}
-//         media={mediaItemUrl}
-//       />
+      <HeroLeftJustified
+        textOverline={textOverline}
+        textHeadline={textHeadline}
+        textParagraph={textParagraph}
+        media={mediaItemUrl}
+      />
 
-//       <SaleCta
-//         isActive={false}
-//         textOverlineTop="THE ANNUAL"
-//         textOverlineBottom="ELIZABETH EAKINS FALL SALE"
-//         textHeadline="Bring Joy In"
-//         textBodyline="SEPTEMBER 20TH - OCTOBER 15TH"
-//         textSublineTop="At the Elizabeth Eakins Studio"
-//         textSublineBottom="5 Taft Street, South Norwalk, CT 06854"
-//         textPostline="BY APPOINTMENT ONLY"
-//         media="images/annualSaleHero.jpg"
-//         textColor="#000000"
-//       />
+      {/* <SaleCta
+        isActive={false}
+        textOverlineTop="THE ANNUAL"
+        textOverlineBottom="ELIZABETH EAKINS FALL SALE"
+        textHeadline="Bring Joy In"
+        textBodyline="SEPTEMBER 20TH - OCTOBER 15TH"
+        textSublineTop="At the Elizabeth Eakins Studio"
+        textSublineBottom="5 Taft Street, South Norwalk, CT 06854"
+        textPostline="BY APPOINTMENT ONLY"
+        media="images/annualSaleHero.jpg"
+        textColor="#000000"
+      /> */}
 
-//       <CallToAction
-//         textHeadline="May Sale 2022"
-//         textParagraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-//         buttonLabel="Shop now"
-//         textColor="#ffffff"
-//         cardBackgroundColor="#6AC283"
-//         media="images/ctaSale.png"
-//         buttonUrl="/rugs"
-//         buttonTextColor="#ffffff"
-//         buttonBackgroundColor="transparent"
-//       />
+      <CallToAction
+        textHeadline="May Sale 2022"
+        textParagraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        buttonLabel="Shop now"
+        textColor="#ffffff"
+        cardBackgroundColor="#6AC283"
+        media="images/ctaSale.png"
+        buttonUrl="/rugs"
+        buttonTextColor="#ffffff"
+        buttonBackgroundColor="transparent"
+      />
 
-//       <ThreeImageButtons
-//         sectionLabel="Explore"
-//         labelTextColor="#587DA9"
-//         textColor="#ffffff"
-//         buttonImageOne="images/rugsLink.png"
-//         labelOne="Rugs"
-//         linkOne={{
-//           url: '/rugs',
-//           target: '_blank',
-//           title: 'Rug Link',
-//           __typename: 'Type Name',
-//         }}
-//         buttonImageTwo="images/fabricLink.png"
-//         labelTwo="Fabric"
-//         linkTwo={{
-//           url: '/fabric',
-//           target: '_blank',
-//           title: 'Rug Link',
-//           __typename: 'Type Name',
-//         }}
-//         buttonImageThree="images/grassclothLink.png"
-//         labelThree="Grasscloth"
-//         linkThree={{
-//           url: '/grasscloth',
-//           target: '_blank',
-//           title: 'Rug Link',
-//           __typename: 'Type Name',
-//         }}
-//       />
+      <ThreeImageButtons
+        sectionLabel="Explore"
+        labelTextColor="#587DA9"
+        textColor="#ffffff"
+        buttonImageOne="images/rugsLink.png"
+        labelOne="Rugs"
+        linkOne={{
+          url: '/rugs',
+          target: '_blank',
+          title: 'Rug Link',
+          __typename: 'Type Name',
+        }}
+        buttonImageTwo="images/fabricLink.png"
+        labelTwo="Fabric"
+        linkTwo={{
+          url: '/fabric',
+          target: '_blank',
+          title: 'Rug Link',
+          __typename: 'Type Name',
+        }}
+        buttonImageThree="images/grassclothLink.png"
+        labelThree="Grasscloth"
+        linkThree={{
+          url: '/grasscloth',
+          target: '_blank',
+          title: 'Rug Link',
+          __typename: 'Type Name',
+        }}
+      />
 
-//       <EmailSubscribe
-//         title="Get new products and promotions in your inbox"
-//         placeholder="Your email"
-//         buttonLabel="Subscribe"
-//         subtitle="Keep up with what we’re up to. Unsubscribe at any time."
-//         textColor="#333333"
-//         backgroundImage="images/emailSubscribe.png"
-//         backgroundColor="#F7F7F7"
-//       />
+      <EmailSubscribe
+        title="Get new products and promotions in your inbox"
+        placeholder="Your email"
+        buttonLabel="Subscribe"
+        subtitle="Keep up with what we’re up to. Unsubscribe at any time."
+        textColor="#333333"
+        backgroundImage="images/emailSubscribe.png"
+        backgroundColor="#F7F7F7"
+      />
 
-//       <ThreeFeatureCards
-//         sectionLabel="Values"
-//         iconLayout={true}
-//         labelTextColor="#587DA9"
-//         textColor="#826C4F"
-//         imageOne="images/sustainability.png"
-//         labelOne="Sustainability"
-//         descriptionOne="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi justo leo, aliquam vitae bibendum ut, dictum sed nulla."
-//         imageTwo="images/naturalFibersBrown.png"
-//         labelTwo="Natural Fibers"
-//         descriptionTwo="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi justo leo, aliquam vitae bibendum ut, dictum sed nulla."
-//         imageThree="images/handmade.png"
-//         labelThree="Handmade"
-//         descriptionThree="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi justo leo, aliquam vitae bibendum ut, dictum sed nulla."
-//       />
+      <ThreeFeatureCards
+        sectionLabel="Values"
+        iconLayout={true}
+        labelTextColor="#587DA9"
+        textColor="#826C4F"
+        imageOne="images/sustainability.png"
+        labelOne="Sustainability"
+        descriptionOne="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi justo leo, aliquam vitae bibendum ut, dictum sed nulla."
+        imageTwo="images/naturalFibersBrown.png"
+        labelTwo="Natural Fibers"
+        descriptionTwo="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi justo leo, aliquam vitae bibendum ut, dictum sed nulla."
+        imageThree="images/handmade.png"
+        labelThree="Handmade"
+        descriptionThree="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi justo leo, aliquam vitae bibendum ut, dictum sed nulla."
+      />
 
-//       <Footer
-//         title="Elizabeth Eakins Studio"
-//         description="The Elizabeth Eakins Studio is located in South Norwalk Connecticut and is open Monday through Friday from 9am to 5:00pm. Please call or email us to visit and we would be happy to have you come by."
-//         address="5 Taft Street, South Norwalk, CT 06854"
-//         phoneNumber="203.831.9347"
-//         email="Elizabeth.Eakins@elizabetheakins.com"
-//         hours="9:00am to 5:00pm M-F"
-//         locationTitle="Holland & Sherry"
-//         locationDescription="Elizabeth Eakins is part of Holland & Sherry and available for purchase across their showrooms. Click below to find a location near you."
-//         locationButtonLabel="Find a location near you"
-//         socialTitle="Follow Elizabeth Eakins on Instagram and join our mailing list"
-//         socialDescription="Many of our new products and offers we first share in email and instagram. Follow us today"
-//         emailPlaceholder="Enter your email"
-//         igCta="Follow us on Instagram"
-//         igLink="https://instagram.com/elizabetheakins/"
-//       />
-//     </>
-//   );
-// }
+      <Footer
+        title="Elizabeth Eakins Studio"
+        description="The Elizabeth Eakins Studio is located in South Norwalk Connecticut and is open Monday through Friday from 9am to 5:00pm. Please call or email us to visit and we would be happy to have you come by."
+        address="5 Taft Street, South Norwalk, CT 06854"
+        phoneNumber="203.831.9347"
+        email="Elizabeth.Eakins@elizabetheakins.com"
+        hours="9:00am to 5:00pm M-F"
+        locationTitle="Holland & Sherry"
+        locationDescription="Elizabeth Eakins is part of Holland & Sherry and available for purchase across their showrooms. Click below to find a location near you."
+        locationButtonLabel="Find a location near you"
+        socialTitle="Follow Elizabeth Eakins on Instagram and join our mailing list"
+        socialDescription="Many of our new products and offers we first share in email and instagram. Follow us today"
+        emailPlaceholder="Enter your email"
+        igCta="Follow us on Instagram"
+        igLink="https://instagram.com/elizabetheakins/"
+      />
+    </>
+  );
+}
 
-// export async function getStaticProps(context: GetStaticPropsContext) {
-//   return getNextStaticProps(context, {
-//     Page,
-//     client,
-//   });
-// }
+export async function getStaticProps(context: GetStaticPropsContext) {
+  return getNextStaticProps(context, {
+    Page,
+    client,
+  });
+}
