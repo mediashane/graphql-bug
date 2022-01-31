f5410810327b02a7a26737222e6c11dee20baa6e
shane-koalition <shane@koalition.com>
2022-01-31 11:59:28 +0000

    sending TEST text'

--- src/pages/[...pageUri].tsx
+++ src/pages/[...pageUri].tsx
@@ -68,7 +68,7 @@ export function PageComponent({ pageUri, clientPageModules }: PageProps) {
   );
   const footerSection = (
     <Footer
-      title="Elizabeth Eakins Studio"
+      title="Elizabeth Eakins Studio TEST"
       description="The Elizabeth Eakins Studio is located in South Norwalk Connecticut and is open Monday through Friday from 9am to 5:00pm. Please call or email us to visit and we would be happy to have you come by."
       address="5 Taft Street, South Norwalk, CT 06854"
       phoneNumber="203.831.9347"
